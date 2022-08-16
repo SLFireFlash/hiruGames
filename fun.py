2640b5fa79edef63cd29c88684d3c370203ec7dc
SLFireFLash <sllahiruprasanna@gmail.com>
2022-08-16 22:41:34 +0530

    dev defs
    gen random qusition and get ans for that qusition

--- fun.py
+++ fun.py
@@ -1,1 +1,27 @@
 import json
+from random import randint
+from re import X
+
+
+
+def Idgen():
+    with open("data/qs.json","r",encoding="utf8")as openfile:
+        qsData =json.load(openfile)
+        qsDataSize = len(qsData)
+        qsId =str(randint(0,qsDataSize))
+        return qsId
+
+
+
+
+def readQs(qsId):
+    with open("data/qs.json","r",encoding="utf8")as openfile:
+        qsData =json.load(openfile)
+        qsDataSize = len(qsData)
+        return qsData[qsId]
+
+def getans(qsId):
+    with open("data/ans.json","r",encoding="utf8")as openfile:
+        ansData =json.load(openfile)
+        ans =ansData[qsId]
+        return ans
